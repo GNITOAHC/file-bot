d4cb0aa7db1a7df564e148c8fc5bdc68ce8c6ba7
GINTOAHC <chaotingchen10@gmail.com>
2024-11-05 00:35:23 +0800

    fix: Static files serving

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use axum::{
-    extract::DefaultBodyLimit,
-    routing::{get, post},
-    Router,
-};
+use axum::{extract::DefaultBodyLimit, routing::post, Router};
 use shuttle_runtime::SecretStore;
+use tower_http::services::ServeDir;
 
 mod discord;
 
 #[derive(Clone)]
 struct AppState {
     discord_info: discord::DiscordInfo,
-}
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
 }
 
 #[shuttle_runtime::main]
@@ -28,8 +21,7 @@ async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum:
     // println!("Discord info: {:?}", state.discord_info);
 
     let router = Router::new()
-        .route("/", get(hello_world))
-        .route("/test", post(hello_world))
+        .nest_service("/", ServeDir::new("assets"))
         .route(
             "/file",
             post(discord::discord::send_file).get(discord::form::send_file_form),
